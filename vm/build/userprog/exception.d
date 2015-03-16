8716cfb8a9df14b0e3157abdc19b547589cb8421
Nikolay Dubina <dubyna.mykola@gmail.com>
2015-03-16 05:50:02 +0300

    lock

--- vm/build/userprog/exception.d
+++ vm/build/userprog/exception.d
@@ -3,5 +3,4 @@ userprog/exception.o: ../../userprog/exception.c \
   ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
   ../../lib/stdbool.h ../../lib/stddef.h ../../lib/kernel/stdio.h \
   ../../userprog/gdt.h ../../threads/loader.h ../../threads/interrupt.h \
-  ../../threads/thread.h ../../lib/kernel/list.h ../../lib/kernel/hash.h \
-  ../../lib/kernel/list.h
+  ../../threads/thread.h ../../lib/kernel/list.h
