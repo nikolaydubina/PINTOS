ac4f1f85ef62bfabf860920e874e6da9a6029255
Nikolay <physman11@gmail.com>
2015-04-18 20:18:04 +0400

    almost empty 2

--- vm/frame.h
+++ vm/frame.h
@@ -15,8 +15,8 @@
 #include "threads/palloc.h"
 #include "vm/page.h"
 
-void frame_table_init(void);
-void* frame_alloc(enum palloc_flags flags, struct page* page);
+void frame_init(void);
+void* frame_create(enum palloc_flags flags, struct page* page);
 void frame_insert(void* addr, struct page* page);
 void frame_free(void* addr);
 
---
+++ vm/swap.c
@@ -0,0 +1,5 @@
+#include "vm/swap.h"
+
+void swap_init(){
+;
+}
