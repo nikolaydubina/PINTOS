5354d996e42ba8246d2d352d08a1b6fa46336b4d
Nikolay <physman11@gmail.com>
2015-04-21 18:04:08 +0400

    TODO. 1. grow stack

--- threads/thread.h
+++ threads/thread.h
@@ -5,6 +5,7 @@
 #include <list.h>
 #include <stdint.h>
 #include "filesys/filesys.h"
+#include "vm/page.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -99,6 +100,8 @@ struct thread
     int exit_status;                    /* exit status of thread */
     struct file* exec_file;
 #endif
+
+    struct page_table* page_table;      /* VM: pagetable descriptor reference */
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
--- vm/page.c
+++ vm/page.c
@@ -1,137 +1,60 @@
 #include "vm/page.h"
 
-static struct page_table_des{
-  struct hash table;
-  tid_t pid;
-
-  struct list_elem elem;
-};
-
-/* global structs */
-static struct lock page_table_list_lock;
-static struct list page_table_list; 
-
-/* pid -> page_table mapping */
-struct hash* get_page_table(tid_t pid);
-struct hash* create_page_table(tid_t pid);
-struct hash* remove_page_table(tid_t pid);
-
 /* helper routines */
-static struct page* page_lookup(struct hash* page_table, const void *address);
+static struct page* page_lookup(const void *address);
 static unsigned page_hash(const struct hash_elem *p_, void *aux UNUSED);
 static bool page_less(const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED);
 
-/* implementation */
-
 void page_init(void){
-  lock_init(&page_table_list_lock);
-  list_init(&page_table_list);
+;
 }
 
+/* initialize page table - per thread */
+void page_construct(void){
+  struct page_table* new = malloc(sizeof(struct page_table));
+
+  hash_init(&new->table, page_hash, page_less, NULL);
+
+  new->thread = thread_current();
+  thread_current()->page_table = new;
+}
+
+/* free page table - per thread */
+void destroy_hentry(struct page* dpage){
+  if (dpage != NULL){
+    frame_free(dpage->vaddr);
+  }
+}
+
+void page_desctruct(void){
+  hash_destroy(&(thread_current()->page_table->table), destroy_hentry);
+  free(thread_current()->page_table);
+}
+
+/* allocate new entry */
 void* page_allocate(enum palloc_flags flags){
-  tid_t curr_pid = thread_current()->tid;
-
-  struct hash* curr_table = get_page_table(curr_pid);
-
-  if (curr_table == NULL)
-    return NULL;
-  
   struct page* new_page = malloc(sizeof(struct page));
   new_page->vaddr = frame_create(flags, new_page); // flags == PAL_USER
   
-  hash_insert(curr_table, &new_page->hash_elem);
+  hash_insert(&(thread_current()->page_table->table), &new_page->hash_elem);
 
   return new_page->vaddr;
 }
 
+/* get pageentry */
 struct page* page_get(void* vaddr){
-  tid_t curr_pid = thread_current()->tid;
-
-  struct page* ret = NULL;
-  struct hash* curr_table = get_page_table(curr_pid);
-  if (curr_table != NULL)
-    ret = page_lookup(curr_table, vaddr);
+  struct page* ret = page_lookup(vaddr);
   return ret;
 }
 
+/* free frame from page-table */
 void page_free(void* vaddr){
-  tid_t curr_pid = thread_current()->tid;
-
-  struct hash* curr_table = get_page_table(curr_pid);
-  if (curr_table != NULL){
-    struct page* curr_page = page_lookup(curr_table, vaddr);
-    if (curr_page != NULL)
-      frame_free(curr_page->vaddr);
+  struct page* curr_page = page_lookup(vaddr);
+  if (curr_page != NULL){
+    frame_free(curr_page->vaddr);
+    hash_delete(&(thread_current()->page_table->table), &curr_page->hash_elem);
   }
 }
-
-/* list of pagetables routines */
-struct hash* get_page_table(tid_t pid){
-  lock_acquire(&page_table_list_lock);
-  struct list_elem* e;
-  struct hash* ret = NULL;
-  for(e = list_begin(&page_table_list);
-      e != list_end(&page_table_list) && ret == NULL;
-      e = list_next(e))
-  {
-    struct page_table_des* curr = list_entry(e, struct page_table_des, elem);
-    if (curr->pid == pid)
-      ret = &curr->table;
-  }
-  lock_release(&page_table_list_lock);
-  return ret;
-}
-
-struct hash* remove_page_table(tid_t pid){
-  lock_acquire(&page_table_list_lock);
-  struct list_elem* e;
-  for(e = list_begin(&page_table_list);
-      e != list_end(&page_table_list);
-      e = list_next(e))
-  {
-    struct page_table_des* curr = list_entry(e, struct page_table_des, elem);
-    if (curr->pid == pid){
-      struct hash* removed = &curr->table;
-      lock_release(&page_table_list_lock);
-
-      /* freeing all entries
-       * allowing other threads manipulate page_table asynchronously
-       */
-      struct hash_iterator i;
-      hash_first (&i, removed);
-      while (hash_next(&i)){
-        struct page* f = hash_entry(hash_cur(&i), struct page, hash_elem);
-        if (f != NULL){
-          /* frame should be thread safe */
-          frame_free(f->vaddr);
-          free(f);
-        }
-      }
-  
-      lock_acquire(&page_table_list_lock);
-      e = list_remove(e);
-      lock_release(&page_table_list_lock);
-      return removed;
-    }
-  }
-  lock_release(&page_table_list_lock);
-  return NULL;
-}
-
-struct hash* create_page_table(tid_t pid){
-  lock_acquire(&page_table_list_lock);
-
-  struct page_table_des* new = malloc(sizeof(struct page_table_des));
-  new->pid = pid;
-  hash_init(&new->table, page_hash, page_less, NULL);
-
-  list_push_back(&page_table_list, &new->elem);
-
-  lock_release(&page_table_list_lock);
-  return &new->table;
-}
-
-/* helper routines */
 
 /* Returns a hash value for page p. */
 static unsigned page_hash(const struct hash_elem *p_, void *aux UNUSED){
@@ -148,11 +71,27 @@ static bool page_less(const struct hash_elem *a_, const struct hash_elem *b_, vo
 
 /* Returns the page containing the given virtual address,
  * or a null pointer if no such page exists. */
-static struct page* page_lookup(struct hash* page_table, const void *vaddress){
+static struct page* page_lookup(const void *vaddress){
   struct page p;
   struct hash_elem *e;
+  struct hash* page_table = &(thread_current()->page_table->table);
 
   p.vaddr = vaddress;
   e = hash_find(page_table, &p.hash_elem);
   return e != NULL ? hash_entry(e, struct page, hash_elem) : NULL;
+}
+
+void grow_stack(void* vaddr){
+  struct page* new_page = malloc(sizeof(struct page));
+  
+  /* TODO */
+  /* som flags */
+  new_page->vaddr = frame_create(flags, new_page); // flags == PAL_USER
+  /* check frame */
+
+  /* install page */
+
+  /* int contxt ?? */
+  
+  hash_insert(&(thread_current()->page_table->table), &new_page->hash_elem);
 }
