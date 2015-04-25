bb3862dae9f5fecbd1b9cc87eaa9f664934c6e9b
Nikolay <physman11@gmail.com>
2015-04-25 14:19:36 +0400

    some stacke growth functionality tests

--- vm/page.h
+++ vm/page.h
@@ -3,18 +3,28 @@
 
 #include <stdlib.h>
 #include <inttypes.h>
-#include <stdio.h>
-#include <string.h>
 #include "kernel/hash.h"
 #include "kernel/list.h"
-#include "threads/synch.h"
 #include "threads/thread.h"
 #include "threads/malloc.h"
 #include "threads/palloc.h"
-#include "vm/frame.h"
+
+enum page_type{
+  PAGE_FILE,
+  PAGE_SWAP,
+  PAGE_MMAP
+};
 
 struct page{
-  void* vaddr;
+  void* vaddr;          /* page,  user,   virual   address */
+  void* paddr;          /* frame, kernel, physical address */
+
+  bool writable;        /* user allowed to write */
+  bool loaded;          /* ready for read/write */
+  bool pinned;          /* true if not in Interrupt context. FIXME*/
+
+  enum page_type type;
+
   struct hash_elem hash_elem;
 };
 
@@ -28,10 +38,11 @@ void page_init(void);
 void page_construct(void);
 void page_destruct(void);
 
-void* page_allocate(enum palloc_flags flags);
-struct page* page_get(void* addr);
-void page_free(void* addr);
+//void* page_allocate(enum palloc_flags flags);
+//struct page* page_get(void* addr);
+//void page_free(void* addr);
 
-void grow_stack(void* vaddr);
+bool load_page(struct page* page);
+bool grow_stack(void* vaddr);
 
 #endif // VM_PAGE
